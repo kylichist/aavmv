e76a0f5e7f0da1670f54417f7904ab4a4d959c6e
kylichist <pimenovpetya@gmail.com>
2020-08-03 18:32:22 +0300

    configure manifest to prevent token stealing

--- app/src/main/java/com/github/kylichist/aavmv/activity/MainActivity.kt
+++ app/src/main/java/com/github/kylichist/aavmv/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.kylichist.aavmv
+package com.github.kylichist.aavmv.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kylichist.aavmv.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
