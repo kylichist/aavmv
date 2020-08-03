3a6c4478f923d7ec01142c0a4bd87adde82b2d3f
kylichist <pimenovpetya@gmail.com>
2020-08-03 19:25:28 +0300

    add toolbar

--- app/src/main/java/com/github/kylichist/aavmv/activity/MainActivity.kt
+++ app/src/main/java/com/github/kylichist/aavmv/activity/MainActivity.kt
@@ -2,11 +2,23 @@ package com.github.kylichist.aavmv.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.github.kylichist.aavmv.R
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val appbar: AppBarLayout = findViewById(R.id.appbar)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        appbar.apply {
+            elevation = 0f
+            outlineProvider = null
+        }
+        toolbar.apply {
+            title = "Token"
+
+        }
     }
 }
