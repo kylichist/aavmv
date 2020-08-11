0c5ca658d2d03c1c23983073794a7612af21173d
kylichist <pimenovpetya@gmail.com>
2020-08-11 17:23:59 +0300

    create request link generator function

--- app/src/main/java/com/github/kylichist/aavmv/util/Util.kt
+++ app/src/main/java/com/github/kylichist/aavmv/util/Util.kt
@@ -81,6 +81,9 @@ fun get(url: String): JSONObject {
     return JSONObject(out)
 }
 
+fun formVkRequestLink(base: String, userToken: String): String = base + ACCESS_TOKEN + userToken + VERSION
+fun formGetUserLink(id: String, userToken: String) = formVkRequestLink(GET_PROFILE_INFO_LINK + id, userToken)
+
 fun String.isValid(): Boolean = Patterns.WEB_URL.matcher(this).matches()
 
 fun String.startIndex(from: String) = this.indexOf(from) + from.length
--- app/src/main/java/com/github/kylichist/aavmv/vk/Call.kt
+++ app/src/main/java/com/github/kylichist/aavmv/vk/Call.kt
@@ -1,12 +1,10 @@
 package com.github.kylichist.aavmv.vk
 
-import com.github.kylichist.aavmv.util.ACCESS_TOKEN
-import com.github.kylichist.aavmv.util.GET_PROFILE_INFO_LINK
-import com.github.kylichist.aavmv.util.VERSION
+import com.github.kylichist.aavmv.util.formGetUserLink
 import com.github.kylichist.aavmv.util.get
 
 fun getName(id: String, userToken: String): String? =
-    with(get(GET_PROFILE_INFO_LINK + id + VERSION + ACCESS_TOKEN + userToken)) {
+    with(get(formGetUserLink(id, userToken))) {
         if (has("response"))
             with(
                 getJSONArray("response")
